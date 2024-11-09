f106d071d8bf38e2a9c585050281617eb5a06b2d
xmu-xiaoma666 <775629340@qq.com>
2024-11-09 21:12:24 +0800

    vMLLM

--- llava/vMLLM_clip_large/maskclip/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-# from .crf import DenseCRF
-from .draw import draw_segmentation
-from .classes import available_datasets, get_classes
-from .palette import available_palette, get_palette
--- llava/vMLLM_siglip_so/maskclip/__init__.py
+++
@@ -1,1 +0,0 @@
-from .maskclip import *
